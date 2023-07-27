dd4eae3099de075885b37ac5e7bb176f8c706508
MayankJindal113 <mjindal1_be21@thapar.edu>
2023-07-28 02:00:22 +0530

    Added a file

---
+++ Basics/Basics.cpp
@@ -0,0 +1,45 @@
+
+
+#include<iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+    int n;
+    cout<<"ENter the number whose factorial is to be calculated: "<<endl;
+    cin>>n;
+    
+
+    
+}
+
+
+/*
+1. Factorial using for loop:
+
+    int ans=1;
+    for(int i=1;i<=n;i++)
+    {   
+        ans=ans*i;
+    }
+    cout<<ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
