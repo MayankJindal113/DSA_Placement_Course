abb849d25ed6354371fb59efc67e2e9a134f9f5b
MayankJindal113 <mjindal1_be21@thapar.edu>
2023-07-29 01:24:18 +0530

    Added few more questions

--- Basics/Basics.cpp
+++ Basics/Basics.cpp
@@ -1,20 +1,35 @@
 
 
 #include<iostream>
+#include<vector>
 using namespace std;
 
+void displ(vector<int> v)
+{
+    int i=0;
+    while (i<v.size())
+    {
+        cout<<v[i]<<" ";
+        i++;
+    }
+    
+}
 
 
 
 int main()
 {
-    int n;
-    cout<<"ENter the number whose factorial is to be calculated: "<<endl;
-    cin>>n;
+    //Week 3 : Sort Color Question 75 leetcode
+    // In-place: means we can't use extra memory in any terms //
     
 
+    vector<int>nums={2,0,2,1,1,0};
     
+
+
 }
+
+
 
 
 /*
@@ -28,18 +43,18 @@ int main()
     cout<<ans;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+VECTORS:
+vector<int>vec1;
+    int i=0,n=5;
+    int j;
+    while (i<n)
+    {   cout<<"Enter the elements : ";
+        cin>>j;
+        vec1.push_back(j);
+        i++;
+    }
+    cout<<endl;
+    displ(vec1);
+    vec1.pop_back();
+    displ(vec1);    
 */
