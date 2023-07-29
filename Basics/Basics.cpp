ec11fc3bc2e3b94dcb78117b36ba46ae78b26401
MayankJindal113 <mjindal1_be21@thapar.edu>
2023-07-29 17:01:14 +0530

    Pointers Started

--- Basics/Basics.cpp
+++ Basics/Basics.cpp
@@ -4,6 +4,38 @@
 #include<vector>
 using namespace std;
 
+
+
+
+int main()
+{
+    
+    int a=10;
+    int * ptr=&a;
+    int *dusraptr=ptr;
+    (*dusraptr)++;
+    cout<<a;
+
+}
+
+
+
+
+/*
+
+1. Factorial using for loop:
+
+    int ans=1;
+    for(int i=1;i<=n;i++)
+    {   
+        ans=ans*i;
+    }
+    cout<<ans;
+-----------------------------------------------------------------------------
+
+VECTORS:
+Ques 75 Leetcode - Sort Colors:(Try using in-space concept- cant use extra memory in any terms)
+
 void displ(vector<int> v)
 {
     int i=0;
@@ -14,37 +46,8 @@ void displ(vector<int> v)
     }
     
 }
-
-
-
-int main()
-{
-    //Week 3 : Sort Color Question 75 leetcode
-    // In-place: means we can't use extra memory in any terms //
-    
-
-    vector<int>nums={2,0,2,1,1,0};
-    
-
-
-}
-
-
-
-
-/*
-1. Factorial using for loop:
-
-    int ans=1;
-    for(int i=1;i<=n;i++)
-    {   
-        ans=ans*i;
-    }
-    cout<<ans;
-
-
-VECTORS:
 vector<int>vec1;
+vector<int>nums={2,0,2,1,1,0};
     int i=0,n=5;
     int j;
     while (i<n)
@@ -57,4 +60,40 @@ vector<int>vec1;
     displ(vec1);
     vec1.pop_back();
     displ(vec1);    
+
+------------------------------------------------------------------------------
+POINTERS:
+  
+    int * aaa;
+    cout<<*aaa;// Bad Practice so should declare nullptr always
+    int * ppt=nullptr;
+    cout<<*ppt;
+
+
+    cout<<sizeof(chh)<<endl;
+    cout<<sizeof(v);
+    cout<<sizeof(p);
+    The size of a pointer is regardless of the type of data it points to as its storing 
+
+
+    cout<<(*ptr)++<<endl;
+    cout<<a<<endl;
+    cout<<ptr++<<endl;
+    cout<<ptr<<endl;
+
+
+    int a=10;
+    int * ptr=&a;
+    (*ptr)=(*ptr)*2;
+    cout<<a;
+    
+    int a=10;
+    int * ptr=&a;
+    int *dusraptr=ptr;
+    (*dusraptr)++;
+    cout<<a;
+
+    
+
+
 */
